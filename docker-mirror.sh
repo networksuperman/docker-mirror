bf9359cf4d05353f25a2d2600c5297c8dbb58a2d
Igor Panarin <59224857+networksuperman@users.noreply.github.com>
2024-05-31 10:45:36 +0300

    add sh

---
+++ docker-mirror.sh
@@ -0,0 +1,14 @@
+cat > /etc/docker/daemon.json <<EOF
+{
+  "log-opts": {
+    "max-size": "500m"
+},
+"registry-mirrors": [
+   "https://dockerhub.timeweb.cloud",
+   "https://dockerhub1.beget.com",
+   "https://mirror.gcr.io"
+]
+}
+EOF
+
+sudo systemctl restart docker
